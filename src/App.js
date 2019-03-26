281a292871a832b687086a3a4c9728f9a761f1a7
rudwnok123 <rudwnok123@naver.com>
2019-03-26 10:58:34 +0900

    repo test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          배포 테스트
+          배포 테스트22
           </p>
           <a
             className="App-link"
