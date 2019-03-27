75e77daa49fc5c1f4dfacbad2845f9e7ef31fb0c
rudwnok123 <rudwnok123@naver.com>
2019-03-27 18:25:47 +0900

    header test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './component/header/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          배포 테스트22
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header/>
       </div>
     );
   }
---
+++ src/component/header/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import './_header.scss';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+      헤더 테스트
+      </header>
+    );
+  }
+}
+
+export default Header;
