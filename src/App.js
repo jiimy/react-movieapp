b92371be1d39518755d0c03c9182885a4b68db37
rudwnok123 <rudwnok123@naver.com>
2019-04-04 18:42:48 +0900

    re test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Header from './component/header/header';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-// import './_header.scss';
+import { Link } from 'react-router-dom';
+import './_header.scss';
 
 class Header extends Component {
   render() {
     return (
       <header>
-      헤더 테스트
+      asdfsadf
       </header>
     );
   }
