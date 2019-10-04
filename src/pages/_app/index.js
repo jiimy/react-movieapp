229a739189b6a9890a24fd6ed8e8f1a1757f6f91
Aiden <rudwnok123@naver.com>
2019-10-04 17:01:12 +0900

    style setting

--- src/pages/_app/index.js
+++ src/pages/_app/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Header from './component/header/header';
+import Header from '../../component/header/header';
 import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
