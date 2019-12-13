446aa3d9687522018a574c4d7a8594061e623b49
rudwn aiden <rudwnok123@naver.com>
2019-12-13 11:14:10 +0900

    hi

---
+++ src/layouts/CommonLayout.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import routerData from '../router/routerData';
+
+class CommonLayout extends Component {
+  render() {
+
+    // const router = renderRou
+    return (
+      <>
+        routerdata 테스트
+      </>
+    );
+  }
+}
+
+export default CommonLayout;
+// export default connect(
+//   ({ auth }) => ({
+//     logged: auth.logged,
+//     user: auth.user,
+//     type: auth.user.type
+//   }),
+//   null
+// )(CommonLayout);
