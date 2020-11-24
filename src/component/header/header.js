28cbe0b2ca84e9c9c4fe4d25ea68cdd913955ac6
rudwnok123 <rudwnok123@naver.com>
2020-11-24 22:05:01 +0900

    push

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -2,22 +2,12 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './_header.scss';
 
-import React from 'react';
-
 const header = () => {
   return (
     <header>
         로그인 ? 로그인 회원정보 : 로그인 회원가입 
         {/* push 테스트 ㅇㅇ*/}
     </header>
-  );
-};
-
-const header = () => {
-  return (
-    <div className="header">
-      
-    </div>
   );
 };
 
